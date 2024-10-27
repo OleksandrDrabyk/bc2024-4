22729fcbfac0b7586b06345a6fcf4cbe450297de
OleksandrDrabyk bc2024-4 <sashadrabyk2017@gmail.com>
2024-10-27 21:53:47 +0200

    реалізував метод GET

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-const { Command } = require('commander');
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const { program } = require('commander');
+const fsPromises = fs.promises;
 
-const program = new Command();
 program
-  .requiredOption('-h, --host <host>', 'address of the server')
-  .requiredOption('-p, --port <port>', 'port of the server')
-  .requiredOption('-c, --cache <cacheDir>', 'path to cache directory')
+  .option('-h, --host <host>', 'address of the server')
+  .option('-p, --port <port>', 'port of the server')
+  .option('-c, --cache <cacheDir>', 'path to cache directory')
   .parse(process.argv);
 
-const { host, port, cache } = program.opts();
+const options = program.opts();
 
-// Веб-сервер
+if (!options.host || !options.port || !options.cache) {
+  console.error("Помилка: не задано обов'язковий параметр.");
+  console.error("Будь ласка, вкажіть усі обов'язкові параметри: --host, --port, --cache.");
+  process.exit(1);
+}
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Server is running');
+  res.end('Сервер працює');
+});
+if (req.method === 'GET') {
+    const fileContent = await fsPromises.readFile(cacheFilePath);
+    res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+    res.end(fileContent);
+
+  }
+server.listen(options.port, options.host, () => {
+  console.log(`Сервер запущено за адресою http://${options.host}:${options.port}/`);
 });
 
-server.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}/`);
-});
